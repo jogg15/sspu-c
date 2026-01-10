d9067d997270a1e643df1e4c30fa0ea3da16105b
jogg15 <jogger.cztechnic@gmail.com>
2026-01-10 11:45:52 +0100

    first commit

---
+++ calc.c
@@ -0,0 +1,33 @@
+#import <stdio.h>
+#import <float.h>
+
+int main () {
+    char znamenko;
+    double vysledek, x, y;
+
+    printf("Zadej znamenko [+, -, *, /]: ");
+    scanf("%c", &znamenko);
+
+    printf("Zadej dve cislice: ");
+    scanf("%lf %lf", &x, &y);
+
+    switch (znamenko) {
+        case '+':
+            vysledek = x + y;
+            break;
+        case '-':
+            vysledek = x - y;
+            break;
+        case '*':
+            vysledek = x * y;
+            break;
+        case '/':
+            vysledek = x / y;
+            break;
+        default:
+            printf("Neplatne znamenko.\n");
+            return 1;
+    }
+        printf("%.2lf", vysledek);
+    return 0;
+}
