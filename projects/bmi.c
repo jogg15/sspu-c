d3f47e82c44e1a71718611b38c63360f9e251540
jogg15 <jogger.cztechnic@gmail.com>
2026-01-10 17:36:08 +0100

    bmi

--- projects/bmi.c
+++ projects/bmi.c
@@ -1,3 +1,19 @@
-//
-// Created by jogg on 1/10/26.
-//
+#include <stdio.h>
+
+int main() {
+    float height;
+    float weight;
+    float vysledek;
+
+    printf("Zadej svoji vysku v metrech: ");
+    scanf("%f", &height);
+
+    printf("Zadej svoji vahu v kg: ");
+    scanf("%f", &weight);
+
+    vysledek = weight / (height * height);
+
+    printf("Tve BMI je: %f", vysledek);
+
+    return 0;
+}
