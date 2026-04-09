dfb34bb6d463ac55442d8b65886249fe2a614d65
Vladimír Studenský <126342834+jogg15@users.noreply.github.com>
2026-04-09 13:16:04 +0200

    to arrays

--- projects/arrays.c
+++ projects/arrays.c
@@ -1,57 +1,57 @@
 #include <stdio.h>
+
 #define SIZE 5
 
-int main (void) {
-    int a = 3, b = 5;
-    printf("a = %d, b = %d\n",a , b);
-    int tmp = a;
-    a = b;
-    b = tmp;
+int main(void) {
+    int values[2] = {3, 5};
 
-    printf("a = %d, b = %d\n",a , b);
+    printf("a = %d, b = %d\n", values[0], values[1]);
 
-    int array[SIZE] = { 1, 2 , 3, 4, 5};
+    int tmp = values[0];
+    values[0] = values[1];
+    values[1] = tmp;
+
+    printf("a = %d, b = %d\n", values[0], values[1]);
+
+    int array[SIZE] = {1, 2, 3, 4, 5};
+
     printf("size of int is %llu bytes.\n", sizeof(int));
-    printf("size of array is %llu bytes.", sizeof(array));
-    printf("Address of the beginning of the array is %p\n", array);
+    printf("size of array is %llu bytes.\n", sizeof(array));
+    printf("Address of the beginning of the array is %p\n", (void*)array);
 
-
-    printf("\n");
-
-
-    for (int i = SIZE-1; i >= 0; i--) {
+    printf("\nOriginal reversed print:\n");
+    for (int i = SIZE - 1; i >= 0; i--) {
         printf("%3d ", array[i]);
     }
 
-    // revers of array elements
-    for (int i  = 0; i < SIZE / 2; i++) {
+    for (int i = 0; i < SIZE / 2; i++) {
         tmp = array[i];
-        array[i] = array[SIZE - 1];
-        array[SIZE - 1] = tmp;
+        array[i] = array[SIZE - 1 - i];
+        array[SIZE - 1 - i] = tmp;
     }
-    printf("Reverse elements:\n ");
 
+    printf("\nReversed array:\n");
     for (int i = 0; i < SIZE; i++) {
         printf("%3d ", array[i]);
     }
     printf("\n");
 
-    int mass = 0;
+    int sum = 0;
     for (int i = 0; i < SIZE; i++) {
-        mass += array[i];
+        sum += array[i];
     }
-    printf("Sum of all elements: %d\n", mass);
 
-    printf("Average of all elements: %f\n", (float)mass / SIZE);
+    printf("Sum of all elements: %d\n", sum);
+    printf("Average of all elements: %f\n", (float)sum / SIZE);
+
     int min = array[0];
     int max = array[0];
 
     for (int i = 1; i < SIZE; i++) {
-        if (array[i] > max ) {
+        if (array[i] > max) {
             max = array[i];
         }
-
-        if (array[i] < min ) {
+        if (array[i] < min) {
             min = array[i];
         }
     }
